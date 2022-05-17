3358b31cf6fa9d581b6fce2bdb42144ebf95ab9c
Your Name <you@example.com>
2022-05-17 13:20:05 +0530

    Second commit

--- src/main/java/restApi02/GetEmployees.java
+++ src/main/java/restApi02/GetEmployees.java
@@ -5,8 +5,15 @@ import static io.restassured.RestAssured.given;
 public class GetEmployees {
 	public static void main(String[] args) {
 		given().log().all()
+	  
+	
 		.when()
 		.get("https://dummy.restapiexample.com/api/v1/employees")
 		.then().log().all();
+		
+		
+		
+		
 	}
 }
+ 
