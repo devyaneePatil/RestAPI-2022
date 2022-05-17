fa923fee56e6a2720b04c12d03bd8c940d4f3506
Your Name <you@example.com>
2022-05-17 13:30:53 +0530

    Requested changes are done

--- src/main/java/restApi02/GetEmployees.java
+++ src/main/java/restApi02/GetEmployees.java
@@ -5,8 +5,6 @@ import static io.restassured.RestAssured.given;
 public class GetEmployees {
 	public static void main(String[] args) {
 		given().log().all()
-	  
-	
 		.when()
 		.get("https://dummy.restapiexample.com/api/v1/employees")
 		.then().log().all();
