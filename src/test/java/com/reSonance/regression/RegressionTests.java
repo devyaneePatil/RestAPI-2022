6e6a63e150da3eba17cb78d3a7f6eb32d020b215
Puneet patil <you@example.com>
2022-05-19 20:27:11 +0530

    my commit

--- src/test/java/com/reSonance/regression/RegressionTests.java
+++ src/test/java/com/reSonance/regression/RegressionTests.java
@@ -16,5 +16,6 @@ public class RegressionTests {
 		//a#w3loginbtn
 	}
 	
+
 				
 }
---
+++ src/test/java/com/reSonance/regression/SanityTest.java
@@ -0,0 +1,14 @@
+package com.reSonance.regression;
+
+import org.testng.annotations.Test;
+
+//import com.reSonance.keywords.UIKeywords;
+
+
+public class SanityTest {
+	@Test
+	public void verifyTitleHomePage() {
+		System.out.println("hello");
+
+	}
+}
